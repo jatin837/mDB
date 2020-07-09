5378b5f2a228ba959fa8fc0f15c03e63d9e544e4
jatin pandey <pandeyjatin47@gmail.com>
2020-07-09 13:42:10 +0530

    using namespace to hook urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+app_name = 'movies'
 urlpatterns = [
     path('', views.index, name = 'moviesindex'),
     path('<int:id>', views.details, name = 'movie-detail'),
